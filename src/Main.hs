665db9c2ee6f873b1d90535fbd854c9ebada1c9f
cassa10 <cassanojoseluis@gmail.com>
2018-11-17 11:34:26 -0300

    Muevo el createMatrix al module Matrix y agrego un comando read que devuelve un Int.

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,17 @@ import MagicTrick
 
 main = do
     c <- readFile "A-small-practice.in"
-    print (lines c)
+    print $ lines c
+    print "Testing reading:"
+    print $ (read ( head (lines c) ) :: Int)
     print "Test 1: "
     print $ getMagicTrick $ Mgt (Argt 1 [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]) (Argt 2 [[1,2,3,6],[5,4,7,8],[9,10,11,12],[13,14,15,16]])
+    writeFile "test.txt"  $ show (read ( head (lines c) ) :: Int)
 
 printCase :: (Show a,Eq a) => Int -> MagicTrick a -> IO()
 printCase n mgt = do
     print $ "Case #" ++ show n ++": " ++ getMagicTrick mgt
+
 {-
 --Pasarlo al module MagicTrick
 toMagicTrick :: [String] -> MagicTrick a
@@ -23,14 +27,3 @@ getSndAgt xs =
     let ys = drop 5 xs in
     Argt (read (head ys)) (createMatrix (tail ys))
 -}
-
-
---Pasarlo al module Matrix
---[String] : valores
--- fst Int: Rows
---snd Int: Columns
---PREC: size [String] = n * m
-createMatrix :: [a] -> Int -> Int -> Matrix a
-createMatrix xs n 0 = []
-createMatrix xs 0 m = []
-createMatrix xs n m =  take m xs : ( createMatrix (drop m xs) (n-1) m)
--- src/Matrix.hs
+++ src/Matrix.hs
@@ -3,6 +3,16 @@ module Matrix where
 --Each index are rows and each index of each row are columns.
 type Matrix a = [[a]]
 
+-- [a] : values
+-- n / fst Int: Rows
+-- m / snd Int: Columns
+--PREC: length [a] >= n * m
+--Prop: Devuelve una matrix de los primeros n*m elementos y 
+createMatrix :: [a] -> Int -> Int -> Matrix a
+createMatrix xs n 0 = []
+createMatrix xs 0 m = []
+createMatrix xs n m =  take m xs : ( createMatrix (drop m xs) (n-1) m)
+
 --Index of Rows starts at 1
 getRow :: Matrix a -> Int -> [a]
 getRow xss 0 = error("Rows doesnt exist")
