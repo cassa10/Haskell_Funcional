3321c91a0e163d20c286c05e3877a1131f2775f0
cassa10 <cassanojoseluis@gmail.com>
2018-11-21 19:40:46 -0300

    Modifico una funcion, agregando un filter cuando los elementos son Strings o Chars. En consecuencia, elimino una funcion que estaba de mas.

--- src/MagicTrick.hs
+++ src/MagicTrick.hs
@@ -4,32 +4,24 @@ import Arrangement
 
 data MagicTrick a = Mgt (Arrangement a) (Arrangement a) deriving Show
 
---Warning: Unexpected return with Strings!! 
+--Hago un filter cuando retorna el result para filtrar el resultado cuando mi Estructura posee Strings o Chars 
 getMagicTrick :: (Show a, Eq a) => MagicTrick a -> String
 getMagicTrick (Mgt argt1 argt2) = 
     let ls = listSameElems (getVolunteerRow argt1) (getVolunteerRow argt2) in
     case length ls of
          0 -> "Volunteer cheated!"
-         1 -> show (head ls)
-         n -> "Bad magician!"
-
-getMagicTrickString :: MagicTrick String -> String
-getMagicTrickString (Mgt argt1 argt2) = 
-    let ls = listSameElems (getVolunteerRow argt1) (getVolunteerRow argt2) in
-    case length ls of
-         0 -> "Volunteer cheated!"
-         1 -> head ls
+         1 -> [x | x <- show(head ls) , x /= '"', x /= '\'' ]
          n -> "Bad magician!"
 
 getCase :: Int -> MagicTrick String -> String
-getCase n mgt = "Case #" ++ show n ++": " ++ getMagicTrickString mgt ++ "\n"
+getCase n mgt = "Case #" ++ show n ++": " ++ getMagicTrick mgt ++ "\n"
 
 -- PREC: Size list of MagicTricks must be <= number of parameter
 getNCases :: Int -> [MagicTrick String] -> String
 getNCases 0 xs = error("Number must be > 0")
 getNCases n [] = error("Size of MagicTricks are not enough.")
 getNCases n xs = getNCases' n 1 xs
-
+ 
 getNCases' :: Int -> Int -> [MagicTrick String] -> String
 getNCases' n 0 xs = error("Iterator must be > 0")
 getNCases' n i [] = error("Size of MagicTricks are not enough.")
